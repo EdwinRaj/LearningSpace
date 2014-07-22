1ff1e60f8aa62c5eb636dc31b1b5d8e44a1b2feb
Edwin.raj <reachedwinraj@gmail.com>
2014-07-22 14:07:42 +0100

    Initial Version on reactive extensions

---
+++ ReactiveExtensionFundamentals/LearnReactiveExtension/Chapter1/BasicRx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reactive.PlatformServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    public class BasicRx
+    {
+        public void NonRxDemo()
+        {
+            
+            var query = from number in Enumerable.Range(1, 5)
+                select number;
+
+            foreach (var i in query)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public void RxDemo()
+        {
+            Console.WriteLine("Main Thread Id-{0}", Thread.CurrentThread.ManagedThreadId);
+
+            var query = from number in Enumerable.Range(1, 5)
+                        select number;
+            var observable = query.ToObservable(NewThreadScheduler.Default);
+            observable.Subscribe(PrintNumber, IamDone);
+            Console.ReadLine();
+        }
+
+        public void ExplicitObserverDemo()
+        {
+            var query = from number in Enumerable.Range(1, 5)
+                        select number;
+            IObservable<int> observable = query.ToObservable();
+            var observer = Observer.Create<int>(PrintNumber,IamDone);
+            observable.Subscribe(observer);
+
+        }
+
+        private static void PrintNumber(int obj)
+        {
+            Console.WriteLine("obj:{0}, Thread Id-{1}",obj,Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private static void IamDone()
+        {
+            Console.WriteLine("I am done");
+        }
+    }
+}
---
+++ ReactiveExtensionFundamentals/LearnReactiveExtension/Chapter1Tests/SimpleRxDemoTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chapter1;
+using NUnit.Framework;
+
+namespace Chapter1Tests
+{
+    [TestFixture]
+    public class SimpleRxDemoTest
+    {
+        [Test]
+        public void BasicDemoTest()
+        {
+            new BasicRx().NonRxDemo();
+        }
+
+        [Test]
+        public void RxDemoTest()
+        {
+            new BasicRx().RxDemo();
+        }
+
+        [Test]
+        public void ExplicitObserverDemoTest()
+        {
+            new BasicRx().ExplicitObserverDemo();
+        }
+    }
+}
