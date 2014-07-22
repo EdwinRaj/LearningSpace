6b2682d9172fdf457b76cf07ecc89d494b389025
Edwin.raj <reachedwinraj@gmail.com>
2014-07-22 17:24:44 +0100

    Chapter 2 initial version

---
+++ ReactiveExtensionFundamentals/LearnReactiveExtension/Chapter2/NumericObserver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter2
+{
+    public class NumericObserver:IObserver<int>
+    {
+        public void OnNext(int value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine(error.ToString());
+        }
+
+        public void OnCompleted()
+        {
+            Console.WriteLine("I am Done!");
+        }
+    }
+}
---
+++ ReactiveExtensionFundamentals/LearnReactiveExtension/Chapter2Tests/NumericObserverTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Linq.ObservableImpl;
+using System.Text;
+using System.Threading.Tasks;
+using Chapter2;
+using NUnit.Framework;
+
+namespace Chapter2Tests
+{
+    [TestFixture]
+    public class NumericObserverTests
+    {
+        [Test]
+        public void TestPrimitiveObserver()
+        {
+            int[] array = {1, 2, 3, 4, 5};
+            var myObservable = (array).ToObservable();
+            var numericObserver = new NumericObserver();
+            myObservable.Subscribe(numericObserver);
+
+        }
+    }
+}
